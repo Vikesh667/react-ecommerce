a5c0c941ebba2a03a70064a8ebd314fee0cdfb38
Vikesh667 <114086050+Vikesh667@users.noreply.github.com>
2023-06-10 16:11:02 +0530

    Routing

---
+++ src/pages/SignUpPages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUp from '../features/Auth/component/SignUp'
+
+const SignUpPages = () => {
+  return (
+    <div>
+      <SignUp/>
+    </div>
+  )
+}
+
+export default SignUpPages
